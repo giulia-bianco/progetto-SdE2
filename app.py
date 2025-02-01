918300f60dff867c56f8db63f912883a5d14066a
giulia-bianco <giulia.bianco.1@studenti.unipd.it>
2025-02-01 21:18:29 +0100

    modificato file app.py, aggiunto preprocessing dati

--- app.py
+++ app.py
@@ -6,3 +6,29 @@ import geopandas as gpd
 url = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/isoc_ci_in_h?format=TSV&compressed=true"
 
 st.write(""" # Utilizzo di Internet nei paesi dell'Unione Europea """)
+
+# pulizia dei dati con polars
+# il dataset originale non è "tidy"
+data = (
+    pl
+    .read_csv(url, separator="\t", null_values=["", ":", ": "])
+    .select(
+        pl.col("freq,unit,hhtyp,geo\\TIME_PERIOD")
+            .str.split(",")
+            .list.to_struct(fields=["freq", "unit", "hhtype", "state"])
+            .alias("orig_col"),
+        pl.col("*").exclude("freq,unit,hhtyp,geo\\TIME_PERIOD"))   
+    .unnest("orig_col")
+    .unpivot(index=["freq", "unit", "hhtype", "state"], value_name="internet_use",variable_name="year")
+    .with_columns(year = pl.col("year")
+        .str.replace(" ", "")
+        .str.replace_all(r'\D', '')
+        .str.replace_all("", "0")
+        .cast(pl.Int64),
+        internet_use = pl.col("internet_use")
+.str.replace(" ", "")
+.str.replace_all(r'\D', '')
+.str.replace_all("", "0")
+.cast(pl.Float64),
+    )    
+)
